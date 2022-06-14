c85a343c2c25eba80a91a74f45139f265fdd99d8
tpkeeper <tpkeeper.me@gmail.com>
2022-06-14 11:13:58 +0800

    bump version to v1.0.0

--- cmd/relay/main.go
+++ cmd/relay/main.go
@@ -72,7 +72,7 @@ func init() {
 	app.Name = "relay"
 	app.Usage = "relay"
 	app.Authors = []*cli.Author{{Name: "Stafi Protocol 2022"}}
-	app.Version = "0.1.3"
+	app.Version = "1.0.0"
 	app.EnableBashCompletion = true
 	app.Commands = []*cli.Command{
 		&accountCommand,
